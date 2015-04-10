9632003effc66c81226b954cc4b44a1db416b5e9
mchukDM <melnichukdmm@gmail.com>
2015-04-11 00:03:52 +0300

    first lab added

---
+++ CryptoAPI/src/cryptoapi/lab1/FirstLab.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cryptoapi.lab1;
+
+import java.security.Provider;
+import java.security.Security;
+
+/**
+ *
+ * @author mchukDM
+ */
+public class FirstLab {
+    private static void showProviders1() {
+        for (Provider provider : Security.getProviders()) {
+            System.out.println("Provider: " + provider + "\ninfo: " + provider.getInfo());
+            int i = 0;
+
+            //for (String key : provider.stringPropertyNames()) {
+            //    System.out.println("\tKey:  " + key + "\t" + provider.getProperty(key));
+            //}
+            for (Provider.Service service : provider.getServices()) {
+                i++;
+                System.out.println(i + "  Algorithm: " + service.getAlgorithm());
+            }
+            System.out.println("\n\n");
+        }
+    }
+}
