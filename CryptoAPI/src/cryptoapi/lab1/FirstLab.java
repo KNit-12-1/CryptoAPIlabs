79ab6ba1c24416edbc921a6c8a9cd44512bc6e68
mchukDM <melnichukdmm@gmail.com>
2015-04-11 00:39:23 +0300

    added type of alg

--- CryptoAPI/src/cryptoapi/lab1/FirstLab.java
+++ CryptoAPI/src/cryptoapi/lab1/FirstLab.java
@@ -13,18 +13,19 @@ import java.security.Security;
  * @author mchukDM
  */
 public class FirstLab {
+
     public static void showProviders1() {
         for (Provider provider : Security.getProviders()) {
             System.out.println("Provider: " + provider + "\ninfo: " + provider.getInfo());
             int i = 0;
 
-            //for (String key : provider.stringPropertyNames()) {
-            //    System.out.println("\tKey:  " + key + "\t" + provider.getProperty(key));
-            //}
+//            for (String key : provider.stringPropertyNames()) {
+//                System.out.println("\tKey:  " + key + "\t" + provider.getProperty(key));
+//            }
             for (Provider.Service service : provider.getServices()) {
                 i++;
-                
-                System.out.println(i + "  Algorithm: " + service.getAlgorithm());
+
+                System.out.println(i + "  Algorithm: " +service.getAlgorithm()+"\tType "+service.getType());
             }
             System.out.println("\n\n");
         }
